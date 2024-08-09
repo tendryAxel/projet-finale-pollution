3a920e533a64c268cee159ae8e4e23857448181b
tendryAxel <hei.tendry@gmail.com>
2024-08-09 10:19:37 +0300

    chore: make the engine of connection singleton

--- exctract_air_pollution.py
+++ exctract_air_pollution.py
@@ -1,27 +1,25 @@
 import pandas as pd
-import sqlalchemy
 import datetime
+
+from sqlalchemy import Engine
 
 from utils import find_city_position, get_pollution, Database
 
-psql_url = Database().create_url()
-engine = sqlalchemy.create_engine(psql_url)
 
-
-def save_hourly_weather_to_csv(city: str, date: datetime.datetime = datetime.datetime.today()) -> None:
+def save_hourly_weather_to_csv(engine: Engine, city: str, date: datetime.datetime = datetime.datetime.today()) -> None:
     location = find_city_position(city)
     pollution = get_pollution(location, date)
     pollution["city_name"] = city
     return pollution.to_sql('weather', con=engine, if_exists='append', index=False)
 
 
-def save_hourly_weather_of_many_to_csv(cities: list[str], date: datetime.datetime = datetime.datetime.today()) -> None:
+def save_hourly_weather_of_many_to_csv(engine: Engine, cities: list[str], date: datetime.datetime = datetime.datetime.today()) -> None:
     city_dataframe = pd.DataFrame({
         "name": cities
     })
     for city in cities:
-        save_hourly_weather_to_csv(city, date)
+        save_hourly_weather_to_csv(engine, city, date)
     city_dataframe.to_sql("city", con=engine, if_exists='replace', index=False)
 
 
-def main(): save_hourly_weather_of_many_to_csv(["Paris", "London"])
+def main(engine: Engine): save_hourly_weather_of_many_to_csv(engine, ["Paris", "London"])
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import dotenv
+import sqlalchemy
 
 import exctract_air_pollution
+from utils import Database
+
+psql_url = Database().create_url()
+engine = sqlalchemy.create_engine(psql_url)
 
 if __name__ == '__main__':
     dotenv.load_dotenv()
-    exctract_air_pollution.main()
+    exctract_air_pollution.main(engine)
