067253e2032d676061186f524ede02cde626c634
tendryAxel <hei.tendry@gmail.com>
2024-08-09 15:20:35 +0300

    chore: rename weather to pollution

--- exctract_air_pollution.py
+++ exctract_air_pollution.py
@@ -6,21 +6,21 @@ from sqlalchemy import Engine
 from utils import find_city_position, get_pollution, Database
 
 
-def save_hourly_weather_to_csv(engine: Engine, city: str, date: datetime.datetime = datetime.datetime.today()) -> None:
+def save_hourly_pollution_to_csv(engine: Engine, city: str, date: datetime.datetime = datetime.datetime.today()) -> None:
     location = find_city_position(city)
     pollution = get_pollution(location, date)
     pollution["city_name"] = city
     pollution["aqi"] = 0
-    return pollution.to_sql('weather_lake', con=engine, if_exists='append', index=False)
+    return pollution.to_sql('pollution_lake', con=engine, if_exists='append', index=False)
 
 
-def save_hourly_weather_of_many_to_csv(engine: Engine, cities: list[str], date: datetime.datetime = datetime.datetime.today()) -> None:
+def save_hourly_pollution_of_many_to_csv(engine: Engine, cities: list[str], date: datetime.datetime = datetime.datetime.today()) -> None:
     city_dataframe = pd.DataFrame({
         "name": cities
     })
     for city in cities:
-        save_hourly_weather_to_csv(engine, city, date)
+        save_hourly_pollution_to_csv(engine, city, date)
     city_dataframe.to_sql("city_lake", con=engine, if_exists='replace', index=False)
 
 
-def main(engine: Engine): save_hourly_weather_of_many_to_csv(engine, ["Paris", "London"])
+def main(engine: Engine): save_hourly_pollution_of_many_to_csv(engine, ["Paris", "London"])
--- main.py
+++ main.py
@@ -6,10 +6,17 @@ import transform_calc_aqi
 from utils import Database
 
 if __name__ == '__main__':
+    # Load .env
     dotenv.load_dotenv()
 
+    # Create database(postgresql) connection
     psql_url = Database().create_url()
     engine = sqlalchemy.create_engine(psql_url)
 
+    # Store air pollution in database with the following pattern
+    # pollution_lake and city_lake as DataLake to store the fetched data
     exctract_air_pollution.main(engine)
+
+    # Calculate the AQI of the stored data
+    # pollution store the AQI based on the POLLUTANT_PM10
     transform_calc_aqi.main(engine)
